2bb309251b87cdbd523f613cf89314d9cd67dce3
Stephan Ellenberger <stephan.ellenberger@justeattakeaway.com>
2024-03-17 20:33:25 +0100

    Parse wave file manually

--- models/parser.rb
+++ models/parser.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'wavefile'
 require_relative 'wave_file'
 require_relative 'xml_builder'
 require 'nokogiri'
@@ -8,9 +7,8 @@ require 'nokogiri'
 module Models
   # A class to parse audio files (linter suggested this comment)
   class Parser
-    def initialize(input_directory, wave_parser = ::WaveFile::Reader, xml_builder = XMLBuilder)
+    def initialize(input_directory, xml_builder = XMLBuilder)
       @input_directory = input_directory
-      @wave_parser = wave_parser
       @xml_builder = xml_builder
     end
 
@@ -20,16 +18,7 @@ module Models
       Dir.glob("#{@input_directory}/*.wav").each do |file|
         no_of_files += 1
 
-        reader = @wave_parser.new(file)
-
-        wave_file = Models::WaveFile.new(
-          file,
-          reader.native_format.audio_format,
-          reader.native_format.channels,
-          reader.native_format.sample_rate,
-          reader.native_format.bits_per_sample,
-          reader.native_format.byte_rate
-        )
+        wave_file = Models::WaveFile.new(file)
 
         @xml_builder.new(wave_file).build_xml_file
       end
--- models/wave_file.rb
+++ models/wave_file.rb
@@ -5,14 +5,9 @@ module Models
   class WaveFile
     attr_reader :file_name, :audio_format, :channel_count, :sampling_rate, :bit_depth, :byte_rate, :bit_rate
 
-    def initialize(file_name, audio_format, channel_count, sampling_rate, bit_depth, byte_rate)
-      @file_name = file_name
-      @audio_format = calculate_audio_format(audio_format)
-      @channel_count = channel_count
-      @sampling_rate = sampling_rate
-      @bit_depth = bit_depth
-      @byte_rate = byte_rate
-      @bit_rate = calculate_bit_rate(sampling_rate, bit_depth, channel_count)
+    def initialize(file)
+      @file_name = file
+      parse
     end
 
     def file_name_without_extension
@@ -21,16 +16,57 @@ module Models
 
     private
 
+    def parse
+      # Source of mapping: https://ccrma.stanford.edu/courses/422-winter-2014/projects/WaveFormat/
+      File.open(file_name, 'rb') do |file|
+        # Seperating the chunks
+        header_chunk = file.read(12)
+
+        raise 'Input file is not a wave file' unless is_a_wave_file?(header_chunk)
+
+        format_chunk = file.read(24)
+
+        @audio_format = calculate_audio_format(format_chunk)
+        @channel_count = get_channel_count(format_chunk)
+        @sampling_rate = get_sampling_rate(format_chunk)
+        @byte_rate = get_byte_rate(format_chunk)
+        @bit_depth = get_bit_depth(format_chunk)
+        @bit_rate = calculate_bit_rate(sampling_rate, bit_depth, channel_count)
+      end
+    end
+
+    def is_a_wave_file?(header_chunk)
+      header_chunk[0..3] == 'RIFF' && header_chunk[8..11] == 'WAVE'
+    end
+
     def calculate_bit_rate(sampling_rate, bit_depth, channel_count)
       sampling_rate * bit_depth * channel_count
     end
 
-    def calculate_audio_format(audio_format)
+    def calculate_audio_format(format_chunk)
+      audio_format = format_chunk[8..9].unpack1('v')
       if audio_format == '1'
         :PCM
       else
         :Compressed
       end
     end
+
+    def get_channel_count(format_chunk)
+      format_chunk[10..11].unpack1('v')
+    end
+
+    def get_sampling_rate(format_chunk)
+      format_chunk[12..15].unpack1('v')
+    end
+
+    def get_byte_rate(format_chunk)
+      format_chunk[16..19].unpack1('V')
+    end
+
+    def get_bit_depth(format_chunk)
+      format_chunk[21..22].unpack1('n')
+    end
+
   end
 end
